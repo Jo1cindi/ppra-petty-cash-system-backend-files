b46b8c505944a844022725ede1d40df030ca2e56
Jo1cindi <joancindi535@gmail.com>
2022-09-26 10:31:11 +0300

    editing index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-require("dotenv").config();
+// require("dotenv").config();
+// const express = require("express");
+// const bodyParser  = require("body-parser");
+// const app = express();
+// const cors = require("cors")
+
+// app.use(cors())
+
+// app.use(bodyParser.json());;
+
+// const employeeRouter  = require("../api/employees/employee.router")
+// app.use("/api/employee", employeeRouter)
+
+// app.listen(process.env.APP_PORT, ()=>{
+//     console.log(`Server is perfectly running on ${process.env.APP_PORT}`)
+// })
 const express = require("express");
-const bodyParser  = require("body-parser");
+
 const app = express();
-const cors = require("cors")
 
-app.use(cors())
+app.get("/", (req, res) => {
+  res.send("Express on Vercel");
+});
 
-app.use(bodyParser.json());;
+app.listen(5000, () => {
+  console.log("Running on port 5000.");
+});
 
-const employeeRouter  = require("../api/employees/employee.router")
-app.use("/api/employee", employeeRouter)
-
-app.listen(process.env.APP_PORT, ()=>{
-    console.log(`Server is perfectly running on ${process.env.APP_PORT}`)
-})
+// Export the Express API
 module.exports = app;
